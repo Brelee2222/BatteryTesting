5f507d4cf9510cdada6a5810fda658551473b3ae
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-28 09:40:05 -0800

    refresh list

--- frontend/batterylist/notesmanage/script.js
+++ frontend/batterylist/notesmanage/script.js
@@ -4,15 +4,26 @@
 
     document.querySelector("#batteryName").innerText = battery.name + " Battery\nNotes Manage";
 
-    document.querySelector("#notesContainer").appendChild(opener.document.querySelector("#notesListScreen").cloneNode(true));
+    function copyNotesList() {
+        document.querySelector("#notesContainer").appendChild(opener.document.querySelector("#notesListScreen").cloneNode(true));
 
-    const noteItems = document.querySelectorAll("#notesListScreen div");
-    noteItems.forEach(note => note.addEventListener("click", () => {
-        noteItems.forEach(node => node.className = "item");
-        note.className = "selected item";
+        const noteItems = document.querySelectorAll("#notesListScreen div");
+        noteItems.forEach(note => note.addEventListener("click", () => {
+            noteItems.forEach(node => node.className = "item");
+            note.className = "selected item";
 
-        noteId = note.getAttribute("noteTime");
-    }));
+            noteId = note.getAttribute("noteTime");
+        }));
+    }
 
-    document.querySelector("#removeNote").addEventListener("click", () => removeNote(noteId));
+    function deleteNote() {
+        removeNote(noteId);
+
+        delete opener.getNotes()[noteId];
+        
+        opener.showNotes();
+        copyNotesList();
+    }
+
+    document.querySelector("#removeNote").addEventListener("click", () => deleteNote);
 }
