aecfb236ec9387b37d0c1a5e3a99247ead605c9d
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-28 09:40:35 -0800

    oopisue

--- frontend/batterylist/notesmanage/script.js
+++ frontend/batterylist/notesmanage/script.js
@@ -20,10 +20,11 @@
         removeNote(noteId);
 
         delete opener.getNotes()[noteId];
-        
+
         opener.showNotes();
         copyNotesList();
     }
 
+    copyNotesList();
     document.querySelector("#removeNote").addEventListener("click", () => deleteNote);
 }
