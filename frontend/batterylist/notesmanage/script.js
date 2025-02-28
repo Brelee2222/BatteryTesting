eed1f7c2862709aeba1e9959389c6e3f9adefdc2
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-27 23:07:26 -0800

    added item classname

--- frontend/batterylist/notesmanage/script.js
+++ frontend/batterylist/notesmanage/script.js
@@ -1,12 +1,14 @@
 {
     const battery = opener.getBattery();
 
-    document.querySelector("#batteryName").innerText = battery.name + " Notes Manage";
+    document.querySelector("#batteryName").innerText = battery.name + " Battery\nNotes Manage";
 
     document.querySelector("#notesContainer").appendChild(opener.document.querySelector("#notesListScreen").cloneNode(true));
 
     document.querySelectorAll("#notesListScreen div").forEach(note => note.addEventListener("click", () => {
-        document.querySelector("#notesListScreen").childNodes.forEach(node => node.className = "");
-        note.className = "selected";
+        document.querySelector("#notesListScreen").childNodes.forEach(node => node.className = "item");
+        note.className = "selected item";
+
+        selectNote(note.noteTime);
     }));
 }
