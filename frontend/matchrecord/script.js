255914a94747198a1e7c1ed3c34e0b3c0bc30995
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-03-03 14:56:05 -0800

    list teams

---
+++ frontend/matchrecord/script.js
@@ -0,0 +1,11 @@
+(async function() {
+    const teamListElement = document.querySelector("#teamNumber");
+
+    for(const team of await (fetch("/BatteryTestingAPI/teams").then(res => res.json()))) {
+        const teamOptionElement = document.createElement("option");
+
+        teamOptionElement.value = team.teamNumber;
+        
+        teamListElement.appendChild(teamOptionElement);
+    }
+})();
