108c4e745a627760702c165c7126caa7d7797817
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-03-28 16:00:01 -0700

    scale test

--- frontend/batterylist/testsGraph.js
+++ frontend/batterylist/testsGraph.js
@@ -1,38 +1,77 @@
-import { getTests, loadTests } from "../utils/test.js";
+import { getTests } from "../utils/test.js";
 
+// Canvas context for test graph
 const graphContext = document.getElementById("testsGraph").getContext("2d");
 
+// Values for generating axes
 const AXIS_PADDING = 20;
 const AXIS_WIDTH = 5;
 
+// Values for generating and connecting points
 const TEST_LINE_WIDTH = 2;
 
-const canvasWidth = graphContext.canvas.width;
-const canvasHeight = graphContext.canvas.height;
+/**
+ * @typedef Scale Graph dimensions
+ * 
+ * @property {number} minX
+ * @property {number} minY
+ * @property {number} maxX
+ * @property {number} maxY
+ */
 
-const graphWidth = canvasWidth - AXIS_PADDING * 2;
-const graphHeight = canvasHeight - AXIS_PADDING * 2;
+/**
+ * @type {Scale}
+ */
+const canvasScale = {
+    minX : 0,
+    minY : 0,
+    maxX : graphContext.canvas.width,
+    maxY : graphContext.canvas.height
+};
 
-let dateFro = 0;
-let dateTo = 0;
+/**
+ * @type {Scale}
+ */
+const graphScale = {
+    minX : AXIS_PADDING,
+    minY : AXIS_PADDING,
+    maxX : canvasDimensions.maxX - AXIS_PADDING,
+    maxY : canvasDimensions.maxY - AXIS_PADDING
+};
 
-let minCapacity = 10;
-let maxCapacity = 90;
+/**
+ * @type {Scale}
+ */
+const testValueScale = {
+    minX : 0,
+    minY : 0,
+    maxX : 0,
+    maxY : 90
+};
 
 // draw graph axes
 function drawAxes() {
     graphContext.lineWidth = AXIS_WIDTH;
-    graphContext.moveTo(AXIS_PADDING, AXIS_PADDING);
-    graphContext.lineTo(AXIS_PADDING, canvasHeight-AXIS_PADDING);
-    graphContext.lineTo(canvasWidth-AXIS_PADDING, canvasHeight-AXIS_PADDING);
+    graphContext.moveTo(graphScale.minX, graphScale.minY);
+    graphContext.lineTo(graphScale.minX, graphScale.maxY);
+    graphContext.lineTo(graphScale.maxX, graphScale.maxY);
     graphContext.stroke();
 }
 
-function testToPoint(test) {
+function transferScale(point, fromScale, toScale) {
     return {
-        x : (test.startTime - dateFro) / (dateTo - dateFro) * graphWidth + AXIS_PADDING,
-        y : (maxCapacity - test.capacity) / (maxCapacity - minCapacity) * graphHeight + AXIS_PADDING
-    };
+        x : (point.x - fromScale.minX) / (fromScale.maxX - fromScale.minX) * (toScale.maxX - toScale.minX) + toScale.minX,
+        y : (point.y - fromScale.minY) / (fromScale.maxY - fromScale.minY) * (toScale.maxY - toScale.minY) + toScale.minY
+    }
+}
+
+function testToPoint(test) {
+    return transferScale({
+        x : test.startTime,
+        y : testValueScale.maxY - test.capacity
+    },
+    testValueScale,
+    graphScale);
 }
 
 export async function drawTests() {
